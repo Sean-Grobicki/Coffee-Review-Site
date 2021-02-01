fd513b35265226ee1be0bc51b808306d8c49541a
Sean-Grobicki <seangrobicki@gmail.com>
2021-02-01 23:01:46 +0000

    Added limited functionality to signup and login

--- components/login.js
+++ components/login.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   Text,
   StatusBar,
+  AsyncStorage,
 } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
@@ -19,21 +20,44 @@ class Login extends Component
     {
       email: '',
       password: '',
+      info: '',
     };
   }
 
   login()
   {
-    this.props.navigation.navigate('Home');
+    return fetch("http://10.0.2.2:3333/user/login",
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            email: this.state.email,
+            password: this.state.password,
+          })
+        })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          Alert.alert(responseJson);
+          this.setState({info: responseJson});
+          //Use this function to store the id and token in async storage.
+          storeData();
+          this.props.navigation.navigate('Home');
+        })
+        .catch((error) => {
+          Alert.alert(error);
+        });
+     }
   }
+
+
 
 
   render()
   {
     return (
       <View style = {styles.container}>
-        <TextInput style = {styles.inputs} placeholder = "Enter your email"/>
-        <TextInput style = {styles.inputs}placeholder = "Enter your password"/>
+        <TextInput style = {styles.inputs} placeholder = "Enter your email" onChangeText = {(email) => this.setState({email:email})}/>
+        <TextInput style = {styles.inputs} secureTextEntry = {true} placeholder = "Enter your password" onChangeText = {(pword) => this.setState({password:pword})}/>
         <TouchableOpacity style = {styles.buttons} onPress = {() => this.login()}>
           <Text>Login</Text>
         </TouchableOpacity>
--- components/signup.js
+++ components/signup.js
@@ -4,21 +4,77 @@ import {
   StyleSheet,
   ScrollView,
   View,
+  TextInput,
+  Button,
+  Alert,
   Text,
   StatusBar,
 } from 'react-native';
+import ValidationComponent from 'react-native-form-validator';
 
-class Signup extends Component
+class Signup extends ValidationComponent
 {
   constructor(props)
   {
     super(props);
+    this.state = 
+    {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      rPassword: '',
+    }
+  }
+  
+  createUser()
+  {
+    this.validate(
+      {
+        firstName: {required:true},
+        lastName: {required: true},
+        email: {required: true, email: true},
+        password: {minLength:5, required: true, equalPassword: this.state.rPassword},
+      });
+      if(this.isFormValid())
+      {
+        return fetch("http://10.0.2.2:3333/user",
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            first_name: this.state.firstName,
+            last_name: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
+          })
+        })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          Alert.alert(responseJson);
+          //this.props.navigation.navigate('Login');
+        })
+        .catch((error) => {
+          Alert.alert(error);
+        });
+     }
+     else
+     {
+       Alert.alert(this.getErrorMessages());
+     }
   }
 
   render()
   {
     return (
-      <Text>Signup</Text>
+      <View>
+          <TextInput placeholder = "Enter First Name" onChangeText = {(fName) => this.setState({firstName:fName})}/>
+          <TextInput placeholder = "Enter Last Name" onChangeText = {(lName) => this.setState({lastName:lName})}/>
+          <TextInput placeholder = "Enter Email" onChangeText = {(email) => this.setState({email:email})}/>
+          <TextInput placeholder = "Enter Password" onChangeText = {(password) => this.setState({password:password})}/>
+          <TextInput placeholder = "Repeat Password" onChangeText = {(rPassword) => this.setState({rPassword:rPassword})}/>
+          <Button title = "Create Account" onPress = {() => this.createUser()}/>
+        </View>
     );
 
   }
